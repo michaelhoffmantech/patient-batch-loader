f5842e5390569c5fca55b5e64a053258f9087b3d
Michael Hoffman <mike@michaelhoffmaninc.com>
2018-03-25 16:54:31 -0500

    Updating to starting point for project dmeo

--- src/main/java/com/pluralsight/springbatch/patientbatchloader/config/ApplicationProperties.java
+++ src/main/java/com/pluralsight/springbatch/patientbatchloader/config/ApplicationProperties.java
@@ -10,21 +10,5 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
 public class ApplicationProperties {
 
-	private final Batch batch = new Batch(); 
-	
-	public Batch getBatch() {
-		return batch;
-	}
-	
-	public static class Batch {
-		private String inputPath = "c:/input";
-		
-		public String getInputPath() {
-			return this.inputPath;
-		}
-		
-		public void setInputPath(String inputPath) {
-			this.inputPath = inputPath; 
-		}
-	}	
+
 }
